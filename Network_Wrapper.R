f1a04a1e9dc1274477d0956792d45c2e48da4c5a
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-05-28 16:29:59 -0400

    Working on light weight networks
    c3net,mrnet and wgcna as input function and still building parameters in config file.

--- Network_Wrapper.R
+++ Network_Wrapper.R
@@ -44,6 +44,46 @@ data = synGet(synID_input, downloadLocation = config$temp_storage_loc)
 net_methods = config$netowrk_method
 net_methods = strsplit(net_methods,",")[[1]]
 
-for (method in net_methods){
-    
+'''
+    Working on only the light networks for now - C3 net, mrnet and WGCNA
+    Just storing the function for reference to delete later
+
+    c3netWrapper  = function(data,path=NULL,pval=1,outputpath){
+  library(c3net)
+  network <- c3net(t(data))
+  #save(network,file=paste0(outputpath,'result_c3net.rda'))
+  network <- network*upper.tri(network)
+  write.csv(network,file=paste0(outputpath,'c3netNetwork.csv'),quote=F)
 }
+
+    mrnetWrapper = function(data,path=NULL,pval=1,outputpath){
+  library(parmigene)                               ########## Need installation in docker
+  metanetwork::aracne(data,path,pval,outputpath)
+  library(data.table)
+  library(dplyr)
+  if(pval==1){
+    fileName <- paste0(outputpath,'aracneNetwork.csv')
+  }else{
+    fileName <- paste0(outputpath,'aracneThresholdNetwork.csv')
+  }
+  cat('fileName:',fileName,'\n')
+  #load(fileName)
+  #data.matrix(data.frame(data.table::fread('~/Desktop/sparrowZNetwork.csv',data.table=F),row.names=1))
+  network <- data.table::fread(fileName,data.table=F) %>%
+    data.frame(row.names=1) %>%
+    data.matrix
+  network <- network+t(network)
+  gc()
+  network <- parmigene::mrnet(data.matrix(network))
+  #save(network,file=paste0(outputpath,'result_mrnet.rda'))
+  network <- network*upper.tri(network)
+  write.csv(network,file=paste0(outputpath,'mrnetNetwork.csv'),quote=F)
+}
+'''
+
+for (method in net_methods){
+    switch(method,
+            "c3net" = )
+}
+
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Reprocessing_Metnetwork_Analysis
 
-This repository is for preparing code and pipeline for the network analysis prior to uploading to forked Metanetwork analysis on Sage Bionetworks.
+## This repository is for preparing code and pipeline for the network analysis prior to uploading to forked Metanetwork analysis on Sage Bionetworks.
+ 
