da9c758002891d69be55f5ea3fd2dc6f583d2398
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-06-01 16:34:31 -0400

    Update Network_Wrapper.R

--- Network_Wrapper.R
+++ Network_Wrapper.R
@@ -16,22 +16,21 @@ library(optparse)
 library(data.table)
 library(parmigene)
 library(WGCNA)
-library(githubr)
 
 # Obtaining the data - From User --------------------------------------------
 
 option_list <- list(make_option(c("-u","--synapse_user"), type="character", action = "store",
-                               help = "Synapse User name"),
-                   make_option(c("-p","--synapse_pass"), type="character", action = "store",
-                               help = "Synapse User Password"),
-                   make_option(c("-p","--config_file"), type="character", action = "store",
-                               help = "Path to the complete config file"))          
+                                help = "Synapse User name"),
+                    make_option(c("-p","--synapse_pass"), type="character", action = "store",
+                                help = "Synapse User Password"),
+                    make_option(c("-p","--config_file"), type="character", action = "store",
+                                help = "Path to the complete config file"))          
 req_args <- parse_args(OptionParser(option_list=option_list))
 
 # Obtaining the data - From Synapse --------------------------------------------
 
 #Setting up the cofig file 
-Sys.setwnd(R_CONFIG_ACTIVE = "default")
+Sys.setenv(R_CONFIG_ACTIVE = "default")
 config <- config::get(file = option_list$config_file)
 
 #Linking with Project
@@ -45,20 +44,23 @@ data = synGet(synID_input, downloadLocation = config$input_profile$temp_storage_
 
 # Performing the analysis -------------------------------------------------
 
-net_methods = config$input_profile$netowrk_method
+net_methods = config$input_profile$network_method
+data = read.csv(data$path)
 
 for (method in net_methods){# Assuming we have more methods - not developing for now
-    switch(method,
-            "c3net" = c3netWrapper(data, path = NULL, pval = config$input_profile$pval, config$output_profile$output_path),# What does this path define in main function?
-            "mrnet" = mrnetWrapper(data, path = NULL, pval = config$input_profile$pval, config$output_profile$output_path),
-            "wgcna" = wgcnaTOM(data, path = NULL, pval = config$input_profile$pval, config$output_profile$output_path, 
-                                config$input_profile$rsquaredcut, config$input_profile$defaultnaPower))
-
+  switch(method,
+         "c3net" = c3netWrapper(data, path = NULL, pval = config$input_profile$pval, config$output_profile$output_path),# What does this path define in main function?
+         "mrnet" = mrnetWrapper(data, path = NULL, pval = config$input_profile$pval, config$output_profile$output_path),
+         "wgcna" = wgcnaTOM(data, path = NULL, pval = config$input_profile$pval, config$output_profile$output_path, 
+                            config$input_profile$rsquaredcut, config$input_profile$defaultnaPower))
+  output_filename <- paste0(config$output_profile$output_path,method,'Network.csv')
+  
+  
 }
 
 # Obtaining the data - For provenance --------------------------------------------
 
-activity <- synapser::synGetEntity(config$input_profile$project_id)
+activity <- synapser::synGet(config$input_profile$project_id)
 
 all.annotations <- list(
   dataType = config$provenance$annotations$data_type,
@@ -76,19 +78,25 @@ all.annotations <- list(
   assay = config$provenance$annotations$assay
 )
 
-thisRepo <- githubr::getRepo(
+thisRepo = NULL
+thisFile = NULL
+
+try(
+  thisRepo <- githubr::getRepo(
   repository = config$provenance$code_annotations$repository,
   ref = config$provenance$code_annotations$ref,
   refName = config$provenance$code_annotations$ref_name
+  ), silent = TRUE
 )
-
+try(
 thisFile <- githubr::getPermlink(
   repository = thisRepo,
   repositoryPath = config$provenance$code_annotations$repository_path
+  ), silent = TRUE
 )
 
 ENRICH_OBJ <- synapser::synStore( synapser::File( 
-  path = config$output_profile$output_path,
+  path = output_filename,
   name = config$output_profile$output_name,
   parentId = activity$properties$id),
   used = config$input_profile$input_synid,
