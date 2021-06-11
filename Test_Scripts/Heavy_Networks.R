104c51b2db013bc5998ac9315dcc6b533e43dabb
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-06-11 13:59:28 -0400

    Update on Test scripts

--- Test_Scripts/Heavy_Networks.R
+++ Test_Scripts/Heavy_Networks.R
@@ -1,4 +1,3 @@
-
 # Calling in Libraries ----------------------------------------------------
 
 library(dplyr, quietly = TRUE)
@@ -34,59 +33,75 @@ req_args <- parse_args(OptionParser(option_list=option_list))
 
 # Obtaining the data - From Synapse --------------------------------------------
 
-#Iterating over every test config file in folder 
+#Setting up the cofig file 
 Sys.setenv(R_CONFIG_ACTIVE = "default")
-config_files = list.files(path = req_args$config_file)
+config <- config::get(file = config_file)
+setwd(config$input_profile$temp_storage_loc)
+  
+#Linking with Project
+synLogin(email = req_args$synapse_user, password = req_args$synapse_pass)
+project = Project(config$input_profile$project_id)
+project <- synStore(project)
+  
+# Data
+synID_input = config$input_profile$input_synid
+data = synGet(synID_input, downloadLocation = config$input_profile$temp_storage_loc)
 
-
-
-for (file_config in config_files){
+dataFolder <- Folder('Light',parent = config$input_profile$project_id)
+dataFolder <- synStore(dataFolder)
   
-  #Setting up the cofig file 
-  file_config_path = paste0(req_args$config_file,file_config)
-  config <- config::get(file = file_config_path)
-  setwd(config$input_profile$temp_storage_loc)
+# Performing the analysis -------------------------------------------------
   
-  #Linking with Project
-  synLogin(email = req_args$synapse_user, password = req_args$synapse_pass)
-  project = Project(config$input_profile$project_id)
-  project <- synStore(project)
+net_methods = c('genie3','tigress')
+data = reader::reader(data$path)
+rows_to_use = (nrow(data)*req_args$percentage_data)/100
+cols_to_use = (ncol(data)*req_args$percentage_data)/100
+data = data[1:rows_to_use,1:cols_to_use]
   
-  # Data
-  synID_input = config$input_profile$input_synid
-  data = synGet(synID_input, downloadLocation = config$input_profile$temp_storage_loc)
-
-  dataFolder <- Folder('Heavy',parent = config$input_profile$project_id)
-  dataFolder <- synStore(dataFolder)
-  
-  # Performing the analysis -------------------------------------------------
-  
-  net_methods = c('genie3','tigress')
-  data = reader::reader(data$path)
-  rows_to_use = (nrow(data)*req_args$percentage_data)/100
-  cols_to_use = (ncol(data)*req_args$percentage_data)/100
-  data = data[1:rows_to_use,1:cols_to_use]
-  
-  if(is.null(config$input_profile$na_fill)){
-    data <- metanetwork::winsorizeData(data)
-  }else{
-    data[is.na(data)] <- config$input_profile$na_fill 
-  }
-  
-  for (method in net_methods){
-    
-    benchMarkRes = mark(switch(method,
-           "genie3" = mpiWrapper(data, nodes = config$computing_specs$heavy_ncores, pathv = NULL, regressionFunction = method,
-                                 outputpath = config$output_profile$output_path),
-           "tigress" = mpiWrapper(data, nodes = config$computing_specs$heavy_ncores, pathv = NULL, regressionFunction = method,
-                                  outputpath = config$output_profile$output_path))
-    ,check=FALSE)
-
-    setwd(config$input_profile$temp_storage_loc)
-    benchmark_filename = paste0(method,"_",as.character(req_args$percentage_data),"_Performance")
-    write.table(benchMarkRes[,1:9],benchmark_filename,quote=F,row.names = T)
-
-    file <- File(path = benchmark_filename, parent = dataFolder)
-    file <- synStore(file)
-  }
+if(is.null(config$input_profile$na_fill)){
+  data <- metanetwork::winsorizeData(data)
+}else{
+  data[is.na(data)] <- config$input_profile$na_fill 
 }
+  
+for (method in net_methods){
+    
+  benchMarkRes = mark(switch(method,
+         "c3net" = c3netWrapper(data, path = NULL, pval = config$input_profile$p_val_c3net, config$output_profile$output_path),# What does this path define in main function?
+         "mrnet" = mrnetWrapper(data, path = NULL, pval = config$input_profile$p_val_mrnet, config$output_profile$output_path),
+         "wgcna" = wgcnaTOM(data, path = NULL, pval = config$input_profile$p_val_wgcna, config$output_profile$output_path, 
+                            config$input_profile$rsquaredcut, config$input_profile$defaultnaPower),
+         "lassoAIC" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                 outputpath = config$output_profile$output_path),
+         "lassoBIC" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                 outputpath = config$output_profile$output_path),
+         "lassoCV1se" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                   outputpath = config$output_profile$output_path),
+         "lassoCVmin" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                   outputpath = config$output_profile$output_path),
+         "ridgeAIC" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                 outputpath = config$output_profile$output_path),
+         "ridgeBIC" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                 outputpath = config$output_profile$output_path),
+         "ridgeCV1se" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                   outputpath = config$output_profile$output_path),
+         "ridgeCVmin" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                   outputpath = config$output_profile$output_path),
+         "sparrowZ" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                 outputpath = config$output_profile$output_path),
+         "sparrow2Z" = mpiWrapper(data, nodes = config$computing_specs$medium_ncores, pathv = NULL, regressionFunction = method,
+                                  outputpath = config$output_profile$output_path),
+         "genie3" = mpiWrapper(data, nodes = config$computing_specs$heavy_ncores, pathv = NULL, regressionFunction = method,
+                               outputpath = config$output_profile$output_path),
+         "tigress" = mpiWrapper(data, nodes = config$computing_specs$heavy_ncores, pathv = NULL, regressionFunction = method,
+                                outputpath = config$output_profile$output_path))
+  
+,check=FALSE)
+  setwd(config$input_profile$temp_storage_loc)
+  benchmark_filename = paste0(method,"_",as.character(req_args$percentage_data),"_Performance.rds")
+  saveRDS(benchMarkRes,benchmark_filename)
+
+  file <- File(path = benchmark_filename, parent = dataFolder)
+  file <- synStore(file)
+}
+
