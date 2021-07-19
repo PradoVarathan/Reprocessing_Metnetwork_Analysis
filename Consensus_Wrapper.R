0b657682849a510e40be1151b5601e4aed3ba4cb
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-07-19 11:15:51 -0400

    update on consensus config file

--- Consensus_Wrapper.R
+++ Consensus_Wrapper.R
@@ -35,14 +35,16 @@ config <- config::get(file = req_args$config_file)
 
 #Linking with Project
 synLogin(email = req_args$synapse_user, password = req_args$synapse_pass)
+input_file = synGet(config$input_profile$input_proj_id,downloadLocation = temp_input_loc)
+fileName = input_file[[1]]$path
 project = Project(config$input_profile$project_id)
 project <- synStore(project)
 
 
 outputpath = config$input_profile$temp_storage_loc
 networkFolderId = config$input_profile$input_folderid
-pattern_id = config$input_profile$pattern_id
-buildConsensus(outputpath = outputpath,networkFolderId = networkFolderId,pattern_id = pattern_id)
+pattern_id = config$input_profile$input_proj_id
+buildConsensus(outputpath = outputpath,networkFolderId = networkFolderId,pattern_id = pattern_id, fileName = fileName)
 
 
 # Obtaining the data - For provenance --------------------------------------------
