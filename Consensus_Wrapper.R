9e59ba42bfe777b7c1a47a21a5b64f817192d9b2
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-09-29 15:25:59 -0400

    Table building in Wrapper

--- Consensus_Wrapper.R
+++ Consensus_Wrapper.R
@@ -44,6 +44,7 @@ project <- synStore(project)
 outputpath = config$input_profile$temp_storage_loc
 networkFolderId = config$input_profile$input_folderid
 pattern_id = config$input_profile$input_proj_id
+
 buildConsensus(outputpath = outputpath,networkFolderId = networkFolderId,pattern_id = pattern_id, fileName = fileName)
 
 
--- Module_Wrapper.R
+++ Module_Wrapper.R
@@ -92,33 +92,41 @@ ga_loc = synGet('syn7806859',downloadLocation = '/home/sage')
 temp_command <- paste0("unzip ",ga_loc$path," -d /home/sage/")
 system(temp_command)
 GANXiS = metanetwork::findModules.GANXiS(adj, '/home/sage/GANXiS_v3.0.2/', nperm = 3, min.module.size = 30)
-
+GANXiS['algorithms'] = 'GANXiS'
 #Fast Greedy Algorithm
 fast_greedy = metanetwork::findModules.fast_greedy(adj, nperm = 3, min.module.size = 30)
-
+fast_greedy['algorithms'] = 'fast_greedy'
 #Label_Prop
 label_prop = metanetwork::findModules.label_prop(adj, nperm = 3, min.module.size = 30)
+label_prop['algorithms'] = 'label_prop'
 
 #Louvain
 louvain = metanetwork::findModules.louvain(adj, nperm = 3, min.module.size = 30)
+louvain['algorithms'] = 'louvain'
 
 #Walktrap
 walktrap = metanetwork::findModules.walktrap(adj, nperm = 3, min.module.size = 30)
+walktrap['algorithms'] = 'walktrap'
 
 #Infomap
 infomap = metanetwork::findModules.infomap(adj, nperm = 3, min.module.size = 30)
+infomap['algorithms'] = 'infomap'
 
 #Link Communities
 linkcommunities = metanetwork::findModules.linkcommunities(adj, nperm = 3, min.module.size = 30)
+linkcommunities['algorithms'] = 'linkcommunities'
 
 #Spinglass
 spinglass = metanetwork::findModules.spinglass(adj, nperm = 3, min.module.size = 30)
+spinglass['algorithms'] = 'spinglass'
 
 #megena
 megena = metanetwork::findModules.megena(adj, method = "pearson", FDR.cutoff = 0.05, module.pval = 0.05, hub.pval = 0.05,doPar = TRUE)
+megena['algorithms'] = 'megena'
 
 #Speakeasy
 #speakeasy_r = metanetwork::findModules.speakeasy(adj)
+#speakeasy_r['algorithms'] = 'speakeasy_r'
 
 algorithms = c('GANXis','fast_greedy','label_prop','louvain','walktrap','infomap','linkcommunities','spinglass','megena')
 # algorithms = c('CFinder', algorithms, 'speakeasy') add after correction of CF issue and rechecking the speakeasy implementation in R
@@ -129,76 +137,88 @@ activity <- synapser::synGet(config$input_profile$project_id)
 
 dataFolder <- Folder(method,parent = config$input_profile$project_id)
 dataFolder <- synStore(dataFolder)
-for (filePath in algorithms){
+for (filenumber in 1:length(algorithms)){
+  filePath = algorithms[filenumber]
   temp_out <- paste0(outputpath,filePath,'.csv')
   write.csv(filePath,temp_out)
   file <- File(path = temp_out, parent = dataFolder)
   file <- synStore(file)
-all.annotations <- synGetAnnotations(config$input_profile$input_synid)
+
+  all.annotations <- synGetAnnotations(config$input_profile$input_synid)
 
 
-checkAnnotations <- function(annotations, config){
-  annot_default <- list(
-    dataType = NULL,
-    resourceType = NULL,
-    metadataType = NULL,
-    isModelSystem = NULL,
-    isMultiSpecimen = NULL,
-    fileFormat = NULL,
-    grant = NULL,
-    species = NULL,
-    organ = NULL,
-    tissue = NULL,
-    study = NULL, 
-    consortium = NULL,
-    assay = NULL
-  )
-  for (item in names(annot_default)){
-    if (!is.null(config$provenance$annotations$item)){
-      annot_default$item = config$provenance$annotations$item[[1]]
-    }
-    else if (!is.null(annotations$item)){
-      annot_default$item = annotations$item[[1]]
+  checkAnnotations <- function(annotations, config){
+    annot_default <- list(
+      dataType = NULL,
+      resourceType = NULL,
+      metadataType = NULL,
+      isModelSystem = NULL,
+      isMultiSpecimen = NULL,
+      fileFormat = NULL,
+      grant = NULL,
+      species = NULL,
+      organ = NULL,
+      tissue = NULL,
+      study = NULL, 
+      consortium = NULL,
+      assay = NULL
+    )
+    for (item in names(annot_default)){
+      if (!is.null(config$provenance$annotations$item)){
+        annot_default$item = config$provenance$annotations$item[[1]]
+      }
+      else if (!is.null(annotations$item)){
+        annot_default$item = annotations$item[[1]]
+      }
     }
   }
-}
 
-all.annotations <- checkAnnotations(all.annotations,config)
+  all.annotations <- checkAnnotations(all.annotations,config)
 
-thisRepo = NULL
-thisFile = NULL
+  thisRepo = NULL
+  thisFile = NULL
 
-try(
-  thisRepo <- githubr::getRepo(
-    repository = config$provenance$code_annotations$repository,
-    ref = config$provenance$code_annotations$ref,
-    refName = config$provenance$code_annotations$ref_name
-  ), silent = TRUE
-)
-try(
-  thisFile <- githubr::getPermlink(
-    repository = thisRepo,
-    repositoryPath = config$provenance$code_annotations$repository_path
-  ), silent = TRUE
-)
+  try(
+    thisRepo <- githubr::getRepo(
+      repository = config$provenance$code_annotations$repository,
+      ref = config$provenance$code_annotations$ref,
+      refName = config$provenance$code_annotations$ref_name
+    ), silent = TRUE
+  )
+  try(
+    thisFile <- githubr::getPermlink(
+      repository = thisRepo,
+      repositoryPath = config$provenance$code_annotations$repository_path
+    ), silent = TRUE
+  )
 
-ENRICH_OBJ <- synapser::synStore( synapser::File( 
-  path = temp_out,
-  name = algorithm,
-  parentId = activity$properties$id),
-  used = config$input_profile$input_synid,
-  activityName = config$provenance$activity_name,
-  executed = thisFile,
-  activityDescription = config$provenance$activity_description
-)
+  ENRICH_OBJ <- synapser::synStore( synapser::File( 
+    path = temp_out,
+    name = algorithm,
+    parentId = activity$properties$id),
+    used = config$input_profile$input_synid,
+    activityName = config$provenance$activity_name,
+    executed = thisFile,
+    activityDescription = config$provenance$activity_description
+  )
 
-synapser::synSetAnnotations(ENRICH_OBJ, annotations = all.annotations)
+  synapser::synSetAnnotations(ENRICH_OBJ, annotations = all.annotations)
 
-md5Command <- paste0('md5sum ', temp_out)
-md5 <- strsplit(system(md5Command, intern = TRUE), '  ')[[1]][1]
-cat(md5, '\n', file = config$output_profile$md5_output_path, sep = '')
+  md5Command <- paste0('md5sum ', temp_out)
+  md5 <- strsplit(system(md5Command, intern = TRUE), '  ')[[1]][1]
+  cat(md5, '\n', file = config$output_profile$md5_output_path, sep = '')
 
-
+  if(filenumber == 1){
+    table <- synBuildTable("Gene Module Result", project, filePath)
+    table <- synStore(table)
+    tableID <- table$tableId
+    #check
+    #table$schema
+  }
+  else{
+    synStore(Table(tableID, filePath))
+  }
+    
 }
 
 
