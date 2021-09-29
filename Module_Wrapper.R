ed89a245d93d7d174af134331f4aa44f0b1d0c67
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-09-29 14:34:56 -0400

    Update Module_Wrapper.R

--- Module_Wrapper.R
+++ Module_Wrapper.R
@@ -118,9 +118,11 @@ spinglass = metanetwork::findModules.spinglass(adj, nperm = 3, min.module.size =
 megena = metanetwork::findModules.megena(adj, method = "pearson", FDR.cutoff = 0.05, module.pval = 0.05, hub.pval = 0.05,doPar = TRUE)
 
 #Speakeasy
+#speakeasy_r = metanetwork::findModules.speakeasy(adj)
 
+algorithms = c('GANXis','fast_greedy','label_prop','louvain','walktrap','infomap','linkcommunities','spinglass','megena')
+# algorithms = c('CFinder', algorithms, 'speakeasy') add after correction of CF issue and rechecking the speakeasy implementation in R
 
-algorithms = c('CFinder','GANXis','fast_greedy','label_prop','louvain','walktrap','infomap','linkcommunities','spinglass')
 # Obtaining the data - For provenance --------------------------------------------
 
 activity <- synapser::synGet(config$input_profile$project_id)
@@ -128,7 +130,81 @@ activity <- synapser::synGet(config$input_profile$project_id)
 dataFolder <- Folder(method,parent = config$input_profile$project_id)
 dataFolder <- synStore(dataFolder)
 for (filePath in algorithms){
-
-  file <- File(path = filePath, parent = dataFolder)
+  temp_out <- paste0(outputpath,filePath,'.csv')
+  write.csv(filePath,temp_out)
+  file <- File(path = temp_out, parent = dataFolder)
   file <- synStore(file)
+all.annotations <- synGetAnnotations(config$input_profile$input_synid)
+
+
+checkAnnotations <- function(annotations, config){
+  annot_default <- list(
+    dataType = NULL,
+    resourceType = NULL,
+    metadataType = NULL,
+    isModelSystem = NULL,
+    isMultiSpecimen = NULL,
+    fileFormat = NULL,
+    grant = NULL,
+    species = NULL,
+    organ = NULL,
+    tissue = NULL,
+    study = NULL, 
+    consortium = NULL,
+    assay = NULL
+  )
+  for (item in names(annot_default)){
+    if (!is.null(config$provenance$annotations$item)){
+      annot_default$item = config$provenance$annotations$item[[1]]
+    }
+    else if (!is.null(annotations$item)){
+      annot_default$item = annotations$item[[1]]
+    }
+  }
+}
+
+all.annotations <- checkAnnotations(all.annotations,config)
+
+thisRepo = NULL
+thisFile = NULL
+
+try(
+  thisRepo <- githubr::getRepo(
+    repository = config$provenance$code_annotations$repository,
+    ref = config$provenance$code_annotations$ref,
+    refName = config$provenance$code_annotations$ref_name
+  ), silent = TRUE
+)
+try(
+  thisFile <- githubr::getPermlink(
+    repository = thisRepo,
+    repositoryPath = config$provenance$code_annotations$repository_path
+  ), silent = TRUE
+)
+
+ENRICH_OBJ <- synapser::synStore( synapser::File( 
+  path = temp_out,
+  name = algorithm,
+  parentId = activity$properties$id),
+  used = config$input_profile$input_synid,
+  activityName = config$provenance$activity_name,
+  executed = thisFile,
+  activityDescription = config$provenance$activity_description
+)
+
+synapser::synSetAnnotations(ENRICH_OBJ, annotations = all.annotations)
+
+md5Command <- paste0('md5sum ', temp_out)
+md5 <- strsplit(system(md5Command, intern = TRUE), '  ')[[1]][1]
+cat(md5, '\n', file = config$output_profile$md5_output_path, sep = '')
+
+
+}
+
+
+# Formatting the network to md5 format --------------------------------------------
+
+
+if((!is.na(config$computing_specs$heavy_ncores)) || (!is.na(config$computing_specs$medium_ncores))){
+  mpi.quit(save = "no")
 }
