94eba32b8b669ad9843680eba49cc69cd15c4d7c
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2022-02-09 00:04:42 -0500

    Update Consensus_Wrapper.R

--- Consensus_Wrapper.R
+++ Consensus_Wrapper.R
@@ -47,6 +47,48 @@ pattern_id = config$input_profile$pattern_id
 network_names = c('c3net','mrnet','wgcna',
                   'lassoAIC','lassoBIC','lassoCV1se','lassoCVmin','ridgeAIC','ridgeBIC','ridgeCV1se','ridgeCVmin',
                   'sparrowZ','sparrow2Z','tigress','genie3')
+
+child_obj <- synapser::synGetChildren(networkFolderId, includeTypes = list('folder'))              
+child_list <- as.list(child_obj)
+child_names = c()
+for(k in 1:length(child_list)){
+    temp_name = child_list[k]
+    temp_name = unlist(temp_name)
+    nn = temp_name['name']
+    nn = as.character(nn)
+    child_names = c(child_names,nn)
+  }
+  #child_names <- lapply(child_list, `[[`, 1)
+  #child_names <- unlist(child_names)
+out_list <- list()
+for (ent in 1:length(child_names)){
+    temp_l = synGetChildren(child_list[[ent]]$id)
+    temp_list = temp_l$asList()
+    temp_names = c()
+    temp_ids = c()
+    for(k in 1:length(temp_list)){
+      temp_name = temp_list[k]
+      temp_name = unlist(temp_name)
+      nn = temp_name['name']
+      nn = as.character(nn)
+      temp_names = c(temp_names,nn)
+      nn = as.character(temp_name['id'])
+      temp_ids = c(temp_ids,nn)
+    }
+    # temp_names <- lapply(temp_list, `[[`, 1)
+    # temp_names <- unlist(temp_names)
+    temp_name_search = paste0(child_names[[ent]],pattern_id)
+    temp_names_ids <- grep(temp_name_search, temp_names)
+    cat(paste0(temp_names[temp_names_ids],' \n'))
+    id_t = temp_ids[temp_names_ids]
+    if(length(id_t) == 0){
+      cat(paste0('No match for ',temp_name_search,' \n'))
+    }else{
+      #cat(paste0(id_t,' \n'))
+      temp <- synGet(id_t, downloadLocation = outputpath)
+      out_list <- append(out_list, temp)
+  }
+bar = out_list
 buildConsensus(outputpath = outputpath,networkFolderId = networkFolderId,pattern_id = pattern_id, fileName = fileName)
 
 
